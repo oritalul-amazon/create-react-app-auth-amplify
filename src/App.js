da148b79c869ee55d677aa99b5e6ac1da78df7ff
orit <oritalul@amazon.com>
2019-03-28 00:19:56 +0200

    Changes to be committed: 	new file:   amplify/backend/analytics/oritapp/parameters.json 	new file:   amplify/backend/analytics/oritapp/pinpoint-cloudformation-template.json 	modified:   amplify/backend/auth/cognitocf0c6096/cognitocf0c6096-cloudformation-template.yml 	modified:   amplify/backend/auth/cognitocf0c6096/parameters.json 	modified:   amplify/backend/backend-config.json 	new file:   amplify/team-provider-info.json 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/images/mygirls.jpeg

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import logo from './images/mygirls.jpeg';
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
+import  { Analytics } from 'aws-amplify';
 import aws_exports from './aws-exports';
+import { Button } from 'aws-amplify-react/dist/AmplifyUI';
 Amplify.configure(aws_exports);
 
 class App extends Component {
+  recordEvent = () => {
+    console.log('recording event to pinpoint!')
+    Analytics.record({
+      name :  'Like Event',
+      attributes : {
+        attr1 : 'attr1' 
+      }
+    })
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} className="App-image" alt="logo" />
+          <p></p>
+          <button onClick={this.recordEvent}>Like</button>
         </header>
       </div>
     );
